8cbe599d07adca1c73c94d7b4ecb6795f7e6623c
MudasirStanikzay <ahmad.mudasir.stanikzay@iths.se>
2025-06-05 23:16:18 +0200

    Building quoteservice, Added QuoteServiceApplication & QuoteServiceApplicationTests

---
+++ quoteservice/src/test/java/se/iths/webservices/quoteservice/QuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.webservices.quoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
